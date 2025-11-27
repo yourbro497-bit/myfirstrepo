bd9eb0f5fb234e88b8d469b88e8eb25e05d04a2a
Karthik <yourbro497@gmail.com>
2025-11-27 14:18:42 -0600

    comments added.

--- myfirstPythonApp.py
+++ myfirstPythonApp.py
@@ -6,3 +6,5 @@ def get_choices():
     
 Choices = get_choices()
 print(Choices)
+
+# This code defines a function that sets the player's choice to "rock" and the computer's choice to "paper". It then returns the computer's choice and prints it.   
